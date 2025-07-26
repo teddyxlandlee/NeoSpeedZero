8f3738ff6a0a7532b229f34160b1e141c62ad197
teddyxlandlee <teddyxlandlee@hotmail.com>
2025-07-26 23:15:08 +0800

    [ci publish] 5.0.2: introduce command `/neospeed view raw`, which returns current speedrun record info in text format

--- common/src/main/java/xland/mcmod/neospeedzero/view/ChallengeSnapshot.java
+++ common/src/main/java/xland/mcmod/neospeedzero/view/ChallengeSnapshot.java
@@ -1,10 +1,13 @@
 package xland.mcmod.neospeedzero.view;
 
 import dev.architectury.networking.NetworkManager;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.UUIDUtil;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentSerialization;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
@@ -16,9 +19,7 @@ import org.jetbrains.annotations.NotNull;
 import xland.mcmod.neospeedzero.record.SpeedrunChallenge;
 import xland.mcmod.neospeedzero.record.SpeedrunRecord;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 public record ChallengeSnapshot(UUID recordId, Component title, List<ItemStack> challenges, long[] successTimeMap) implements CustomPacketPayload {
     // The exact time is not used yet. We simply check its non-negativity.
@@ -85,5 +86,51 @@ public record ChallengeSnapshot(UUID recordId, Component title, List<ItemStack> 
         public @NotNull Type<Change> type() {
             return ViewPackets.TYPE_CHANGE;
         }
+    }
+
+    public Component toText() {
+        final MutableComponent root = Component.empty();
+        root.append(Component.translatable(
+                "command.neospeedzero.view_raw.title", title()
+        ).withStyle(s -> s.withHoverEvent(new HoverEvent.ShowText(
+                Component.translatable("message.neospeedzero.record.snapshot.id", String.valueOf(recordId()))
+        )))).append("\n");
+
+        final List<ItemStack> yes = new ArrayList<>();
+        final List<ItemStack> no = new ArrayList<>();
+        {
+            int i = 0;
+            final long[] timeMap = successTimeMap();
+            for (ItemStack challenge : challenges()) {
+                List<ItemStack> target = timeMap[i++] >= 0 ? yes : no;
+                target.add(challenge);
+            }
+        }
+
+        // Yes
+        root.append(Component.translatable(
+                "command.neospeedzero.view_raw.complete.yes", yes.size()
+        ).withStyle(ChatFormatting.GREEN)).append("\n");
+        root.append(join(yes.iterator()));
+        root.append("\n");
+        // No
+        root.append(Component.translatable(
+                "command.neospeedzero.view_raw.complete.no", no.size()
+        ).withStyle(ChatFormatting.RED)).append("\n");
+        root.append(join(no.iterator()));
+
+        root.append("\n\n").append(Component.translatable("message.neospeedzero.record.snapshot.non-synced"));
+
+        return root;
+    }
+
+    private static Component join(Iterator<ItemStack> stacks) {
+        final MutableComponent component = Component.empty();
+        while (stacks.hasNext()) {
+            ItemStack next = stacks.next();
+            component.append(next.getDisplayName());
+            if (stacks.hasNext()) component.append(", ");
+        }
+        return component;
     }
 }
