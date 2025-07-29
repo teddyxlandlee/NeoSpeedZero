820f7d332697959f0b6c32de1b2673b0d6b2c7d8
teddyxlandlee <teddyxlandlee@hotmail.com>
2025-07-29 12:39:35 +0800

    [ci build] adds several extra requirements for item predicate

--- common/src/main/java/xland/mcmod/neospeedzero/resource/ExtraRequirements.java
+++ common/src/main/java/xland/mcmod/neospeedzero/resource/ExtraRequirements.java
@@ -2,6 +2,7 @@ package xland.mcmod.neospeedzero.resource;
 
 import com.google.common.base.Preconditions;
 import com.mojang.datafixers.util.Either;
+import net.minecraft.advancements.critereon.CollectionPredicate;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.advancements.critereon.MinMaxBounds;
 import net.minecraft.core.Holder;
@@ -9,22 +10,26 @@ import net.minecraft.core.HolderSet;
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.core.component.predicates.DamagePredicate;
-import net.minecraft.core.component.predicates.DataComponentPredicate;
-import net.minecraft.core.component.predicates.DataComponentPredicates;
+import net.minecraft.core.component.predicates.*;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.JukeboxSong;
+import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.component.ItemLore;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.world.item.equipment.trim.TrimMaterial;
+import net.minecraft.world.item.equipment.trim.TrimPattern;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 import xland.mcmod.neospeedzero.mixin.EnchantmentsPredicateAccessor;
 
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 final class ExtraRequirements {
     static void fillExtraRequirements(ItemStack stack,
@@ -39,9 +44,14 @@ final class ExtraRequirements {
             if (!count.isAny())
                 appendedLores.add(Component.translatable("item_predicate.neospeedzero.extra_req.count", formatIntRange(count)));
 
+            // Component
+
             DataComponentPatch componentChanges = predicate.components().exact().asPatch();
             Map<DataComponentPredicate.Type<?>, DataComponentPredicate> partial = predicate.components().partial();
-            // Damage
+            stack.applyComponents(componentChanges);
+
+            // Damage - [E,P]minecraft:damage
+            stack.remove(DataComponents.DAMAGE);
             componentFromChanges(componentChanges, DataComponents.DAMAGE).ifPresentOrElse(damage -> {
                 // Exact damage predicate
                 appendedLores.add(Component.translatable("item_predicate.neospeedzero.extra_req.damage", formatNumber(damage, damage)));
@@ -53,13 +63,56 @@ final class ExtraRequirements {
                     appendedLores.add(Component.translatable("item_predicate.neospeedzero.extra_req.damage", formatIntRange(damage)));
                 }
             });
-            // Enchantments
-            readEnchantments(componentChanges, DataComponents.STORED_ENCHANTMENTS, partial.get(DataComponentPredicates.STORED_ENCHANTMENTS), appendedLores::add);
-            readEnchantments(componentChanges, DataComponents.ENCHANTMENTS, partial.get(DataComponentPredicates.ENCHANTMENTS), appendedLores::add);
+            // Enchantments - [E,P]minecraft:stored_enchantments, minecraft:enchantments
+            readEnchantments(partial.get(DataComponentPredicates.STORED_ENCHANTMENTS), appendedLores::add);
+            readEnchantments(partial.get(DataComponentPredicates.ENCHANTMENTS), appendedLores::add);
 
-            // Custom Data
+            // Bundle Contents - [P]minecraft:bundle_contents
+            // [E]minecraft:bundle_contents will be handled by ItemStack.components
+            if (partial.get(DataComponentPredicates.BUNDLE_CONTENTS) instanceof BundlePredicate(
+                    Optional<CollectionPredicate<ItemStack, ItemPredicate>> items
+            ) && items.isPresent()) {
+                // Too complicated
+                // Similar for [P]minecraft:container
+                appendedLores.add(Component.translatable("item_predicate.neospeedzero.extra_req.bundle"));
+            }
 
-            // TODO: fill other extra requirements
+            // Custom Data - [E,P]minecraft:custom_data
+            if (partial.containsKey(DataComponentPredicates.CUSTOM_DATA) || componentFromChanges(componentChanges, DataComponents.CUSTOM_DATA).isPresent()) {
+                appendedLores.add(Component.translatable("item_predicate.neospeedzero.extra_req.custom_data"));
+            }
+
+            // TODO: [E,P] firework_explosion, fireworks, writable_book_contents, written_book_contents
+
+            // Music - minecraft:jukebox_playable
+            if (partial.get(DataComponentPredicates.JUKEBOX_PLAYABLE) instanceof JukeboxPlayablePredicate(
+                    Optional<HolderSet<JukeboxSong>> song
+            ) && song.isPresent()) {
+                appendHomogenousSet(
+                        Component.translatable("item_predicate.neospeedzero.extra_req.song"),
+                        song.get(),
+                        appendedLores::add
+                );
+            }
+
+            if (partial.get(DataComponentPredicates.POTIONS) instanceof PotionsPredicate(HolderSet<Potion> potions)) {
+                appendHomogenousSet(Component.translatable("item_predicate.neospeedzero.extra_req.potion"), potions, appendedLores::add);
+            }
+
+            if (partial.get(DataComponentPredicates.ARMOR_TRIM) instanceof TrimPredicate(
+                    Optional<HolderSet<TrimMaterial>> material, Optional<HolderSet<TrimPattern>> pattern
+            )) {
+                material.ifPresent(holders -> appendHomogenousSet(
+                        Component.translatable("item_predicate.neospeedzero.extra_req.trim.material"), holders, appendedLores::add)
+                );
+                pattern.ifPresent(holders -> appendHomogenousSet(
+                        Component.translatable("item_predicate.neospeedzero.extra_req.trim.patterns"), holders, appendedLores::add
+                ));
+            }
+        }
+
+        if (ofAny != null) {
+            appendHomogenousSet(Component.translatable("item_predicate.neospeedzero.extra_req.items"), ofAny, appendedLores::add);
         }
 
         if (!appendedLores.isEmpty()) {
@@ -72,19 +125,8 @@ final class ExtraRequirements {
         }
     }
 
-    private static void readEnchantments(DataComponentPatch componentChanges,
-                                         DataComponentType<ItemEnchantments> componentType,
-                                         @Nullable DataComponentPredicate componentPredicate,
+    private static void readEnchantments(@Nullable DataComponentPredicate componentPredicate,
                                          Consumer<Component> loreAdder) {
-        componentFromChanges(componentChanges, componentType).ifPresent(enchantments ->
-                enchantments.entrySet().forEach(entry -> {
-                    Holder<Enchantment> enchantmentType = entry.getKey();
-                    int enchantmentLevel = entry.getIntValue();
-                    // Name
-                    loreAdder.accept(Component.empty().append(enchantmentType.value().description())
-                                    .append(" ")
-                                    .append(formatNumber(enchantmentLevel, enchantmentLevel)));
-        }));
         if (componentPredicate instanceof EnchantmentsPredicateAccessor enchantmentsPredicate) {
             enchantmentsPredicate.ns0$getEnchantments().forEach(enchantmentPredicate -> {
                 Optional<HolderSet<Enchantment>> enchantments = enchantmentPredicate.enchantments();
@@ -120,9 +162,31 @@ final class ExtraRequirements {
         return Component.translatable("item_predicate.neospeedzero.extra_req.count.between", min, max);
     }
 
+    @SuppressWarnings("OptionalAssignedToNull") // This is what Stupid Mojang does
     private static <T> Optional<? extends T> componentFromChanges(DataComponentPatch changes, DataComponentType<? extends T> componentType) {
         Optional<? extends T> t = changes.get(componentType);
         if (t == null /*sic*/) return Optional.empty();
         return t;
+    }
+
+    private static <T> Stream<String> formatHomogenousSet(Either<TagKey<T>, HolderSet<T>> unwrapped) {
+        return unwrapped.map(
+                tagKey -> Stream.of("#" + tagKey.location()),
+                holders -> holders.stream().map(Holder::getRegisteredName)
+        );
+    }
+
+    private static <T> void appendHomogenousSet(
+            MutableComponent prefix, Either<TagKey<T>, HolderSet<T>> unwrapped,
+            Consumer<Component> loreAdder
+    ) {
+        final String text = formatHomogenousSet(unwrapped).map(s -> "\n - " + s).collect(Collectors.joining());
+        loreAdder.accept(prefix.append(text));
+    }
+
+    private static <T> void appendHomogenousSet(
+            MutableComponent prefix, HolderSet<T> wrapped, Consumer<Component> loreAdder
+    ) {
+        appendHomogenousSet(prefix, wrapped.unwrap().mapRight(HolderSet::direct), loreAdder);
     }
 }
